354f9c611a2971af927e2dd9c218d2f12cdfb78e
setosandoval <s.sandovalc2@uniandes.edu.co>
2024-09-28 16:25:35 -0500

    data

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Repositorio Talleres Grupo 4 - Economía Urbana 2024
 
-Este es el repositorio con la solución de los talleres y el trabajo final para Economía Urbana en 2024-20. Los autores son el el grupo 4 conformado por Sergio Sandoval y María Fernanda Blanco.
+Este es el repositorio con la solución de los talleres y el trabajo final para Economía Urbana en 2024-20. Los autores son el grupo 4, conformado por Sergio Sandoval y María Fernanda Blanco.
 
-El repositorio contiene 4 carpetas. Una con el `trabajo final` y otras 3 el contenido correspondiente a la solución de los `talleres` 3, 4 y 5. 
+El repositorio contiene 4 carpetas. Una con el `trabajo final` y otras 3 con el contenido correspondiente a la solución de los `talleres` 3, 4 y 5. 
 
 En cada carpeta es posible encontrar:
 
---
+++ Taller 3/scripts/punto1_T3.R
@@ -0,0 +1,20 @@
+# ========================================================================== #
+# Taller 3                                                                   #
+# Economía Urbana - 202420                                                   #
+#                                                                            #
+# Script: Punto 1                                                           #
+#                                                                            #
+# Grupo 4: - Sergio Sandoval                                                 #
+#          - María Fernanda Blanco                                           #
+# ========================================================================== #
+
+
+# CONFIGURACIÓN DEL ENTORNO Y CARGA DE PAQUETES ==============================
+rm(list = ls())
+
+#  Directorio 
+setwd("/Users/setosandoval/Desktop/Urbana/Talleres Repo/Econ_Urbana_Grupo4_2024/...")
+
+# Paquetes
+library(pacman)
+p_load()  
